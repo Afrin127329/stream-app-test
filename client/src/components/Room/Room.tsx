adf8210367509ac913bf43ae4bbbd0b18436956c
Afrin Nahar <afrinnahar1999@gmail.com>
2024-06-28 19:50:55 +0600

    updated cloudflare turn server

--- client/src/components/Room/Room.tsx
+++ client/src/components/Room/Room.tsx
@@ -16,50 +16,62 @@ import JoinRoom from "../JoinRoom/JoinRoom";
 import "./Room.css";
 
 // Global variables
-const serverUrl = "https://meet.jobabd.xyz";
-// const serverUrl = "https://stream-app-test-us4j.onrender.com";
-// const configuration = {
-//     iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
-// };
+// const serverUrl = "https://meet.jobabd.xyz";
+const serverUrl = "https://stream-app-test-us4j.onrender.com";
 
 const configuration = {
   iceServers: [
-    { urls: "stun:stun01.sipphone.com" },
-    { urls: "stun:stun.ekiga.net" },
-    { urls: "stun:stun.fwdnet.net" },
-    { urls: "stun:stun.ideasip.com" },
-    { urls: "stun:stun.iptel.org" },
-    { urls: "stun:stun.rixtelecom.se" },
-    { urls: "stun:stun.schlund.de" },
-    { urls: "stun:stun.l.google.com:19302" },
-    { urls: "stun:stun1.l.google.com:19302" },
-    { urls: "stun:stun2.l.google.com:19302" },
-    { urls: "stun:stun3.l.google.com:19302" },
-    { urls: "stun:stun4.l.google.com:19302" },
-    { urls: "stun:stunserver.org" },
-    { urls: "stun:stun.softjoys.com" },
-    { urls: "stun:stun.voiparound.com" },
-    { urls: "stun:stun.voipbuster.com" },
-    { urls: "stun:stun.voipstunt.com" },
-    { urls: "stun:stun.voxgratia.org" },
-    { urls: "stun:stun.xten.com" },
     {
-      urls: "turn:numb.viagenie.ca",
-      credential: "muazkh",
-      username: "webrtc@live.com",
-    },
-    {
-      urls: "turn:192.158.29.39:3478?transport=udp",
-      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-      username: "28224511:1379330808",
-    },
-    {
-      urls: "turn:192.158.29.39:3478?transport=tcp",
-      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-      username: "28224511:1379330808",
+      urls: [
+        "stun:stun.cloudflare.com:3478",
+        "turn:turn.cloudflare.com:3478?transport=udp",
+        "turn:turn.cloudflare.com:3478?transport=tcp",
+        "turns:turn.cloudflare.com:5349?transport=tcp",
+      ],
+      credential: `${process.env.CLOUDFLARE_TURN_CREDENTIAL}`,
+      username: `${process.env.CLOUDFLARE_TURN_USERNAME}`,
     },
   ],
 };
+
+// working servers
+// iceServers: [
+//   { urls: "stun:stun01.sipphone.com" },
+//   { urls: "stun:stun.ekiga.net" },
+//   { urls: "stun:stun.fwdnet.net" },
+//   { urls: "stun:stun.ideasip.com" },
+//   { urls: "stun:stun.iptel.org" },
+//   { urls: "stun:stun.rixtelecom.se" },
+//   { urls: "stun:stun.schlund.de" },
+//   { urls: "stun:stun.l.google.com:19302" },
+//   { urls: "stun:stun1.l.google.com:19302" },
+//   { urls: "stun:stun2.l.google.com:19302" },
+//   { urls: "stun:stun3.l.google.com:19302" },
+//   { urls: "stun:stun4.l.google.com:19302" },
+//   { urls: "stun:stunserver.org" },
+//   { urls: "stun:stun.softjoys.com" },
+//   { urls: "stun:stun.voiparound.com" },
+//   { urls: "stun:stun.voipbuster.com" },
+//   { urls: "stun:stun.voipstunt.com" },
+//   { urls: "stun:stun.voxgratia.org" },
+//   { urls: "stun:stun.xten.com" },
+//   {
+//     urls: "turn:numb.viagenie.ca",
+//     credential: "muazkh",
+//     username: "webrtc@live.com",
+//   },
+//   {
+//     urls: "turn:192.158.29.39:3478?transport=udp",
+//     credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+//     username: "28224511:1379330808",
+//   },
+//   {
+//     urls: "turn:192.158.29.39:3478?transport=tcp",
+//     credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+//     username: "28224511:1379330808",
+//   },
+// ],
+
 let connections: any = {};
 let socket: Socket | null = null;
 let socketId: string | any = null;
