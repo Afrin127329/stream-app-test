7b35f86fed736381eec40ab786f96b58a79806f2
Afrin Nahar <afrinnahar1999@gmail.com>
2024-07-05 12:38:09 +0600

    local set up for the server url

--- client/src/components/Room/Room.tsx
+++ client/src/components/Room/Room.tsx
@@ -16,10 +16,25 @@ import JoinRoom from "../JoinRoom/JoinRoom";
 import "./Room.css";
 
 // Global variables
-const serverUrl = `${
-  document.location.protocol + "//" + document.location.host
-}`;
+// const serverUrl = `${
+//   document.location.protocol + "//" + document.location.host
+// }`;
+const serverUrl = "http://localhost:8000";
 
+// const configuration = {
+//   iceServers: [
+//     {
+//       urls: [
+//         "stun:stun.cloudflare.com:3478",
+//         "turn:turn.cloudflare.com:3478?transport=udp",
+//         "turn:turn.cloudflare.com:3478?transport=tcp",
+//         "turns:turn.cloudflare.com:5349?transport=tcp",
+//       ],
+//       credential: `${process.env.CLOUDFLARE_TURN_CREDENTIAL}`,
+//       username: `${process.env.CLOUDFLARE_TURN_USERNAME}`,
+//     },
+//   ],
+// };
 const configuration = {
   iceServers: [
     {
@@ -29,49 +44,11 @@ const configuration = {
         "turn:turn.cloudflare.com:3478?transport=tcp",
         "turns:turn.cloudflare.com:5349?transport=tcp",
       ],
-      credential: `${process.env.CLOUDFLARE_TURN_CREDENTIAL}`,
-      username: `${process.env.CLOUDFLARE_TURN_USERNAME}`,
+      credential: `${import.meta.env.CLOUDFLARE_TURN_CREDENTIAL}`,
+      username: `${import.meta.env.CLOUDFLARE_TURN_USERNAME}`,
     },
   ],
 };
-
-// working servers
-// iceServers: [
-//   { urls: "stun:stun01.sipphone.com" },
-//   { urls: "stun:stun.ekiga.net" },
-//   { urls: "stun:stun.fwdnet.net" },
-//   { urls: "stun:stun.ideasip.com" },
-//   { urls: "stun:stun.iptel.org" },
-//   { urls: "stun:stun.rixtelecom.se" },
-//   { urls: "stun:stun.schlund.de" },
-//   { urls: "stun:stun.l.google.com:19302" },
-//   { urls: "stun:stun1.l.google.com:19302" },
-//   { urls: "stun:stun2.l.google.com:19302" },
-//   { urls: "stun:stun3.l.google.com:19302" },
-//   { urls: "stun:stun4.l.google.com:19302" },
-//   { urls: "stun:stunserver.org" },
-//   { urls: "stun:stun.softjoys.com" },
-//   { urls: "stun:stun.voiparound.com" },
-//   { urls: "stun:stun.voipbuster.com" },
-//   { urls: "stun:stun.voipstunt.com" },
-//   { urls: "stun:stun.voxgratia.org" },
-//   { urls: "stun:stun.xten.com" },
-//   {
-//     urls: "turn:numb.viagenie.ca",
-//     credential: "muazkh",
-//     username: "webrtc@live.com",
-//   },
-//   {
-//     urls: "turn:192.158.29.39:3478?transport=udp",
-//     credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-//     username: "28224511:1379330808",
-//   },
-//   {
-//     urls: "turn:192.158.29.39:3478?transport=tcp",
-//     credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-//     username: "28224511:1379330808",
-//   },
-// ],
 
 let connections: any = {};
 let socket: Socket | null = null;
